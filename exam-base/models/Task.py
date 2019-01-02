a990b30e3de4bdd78d697419051c3e8aa6ba2b8c
ApolloOne <trang78952@gmail.com>
2019-01-02 10:56:26 +0700

    modifed field project and task

--- exam-base/models/Task.py
+++ exam-base/models/Task.py
@@ -5,7 +5,11 @@ from odoo import fields, models, api
 class Task (models.Model):
     _name = 'exam.task'
     name = fields.Char(string="Name task")
-    user_id = fields.Many2one('res.users')
-    start_date = fields.Datetime()
-    due_date = fields.Datetime()
-    project_id=fields.Many2one("exam.project")
+    user_id = fields.Many2one(comodel_name='res.users',
+                              string='nguoi thuc hien',
+                              )
+    start_date = fields.Date()
+    due_date = fields.Date()
+    project_id = fields.Many2one(comodel_name="exam.project",
+                                 string=u'Project',
+                                 )
--- exam-base/models/project.py
+++ exam-base/models/project.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
-from odoo import fields, models, api
+from odoo import fields, models, api, exceptions
 
 
 class Project (models.Model):
     _name = 'exam.project'
-    name = fields.Char('Name Project')
+    name = fields.Char(string='Name Project')
     manager_user_id = fields.Char()
-    start_date = fields.Datetime()
-    due_date = fields.Datetime()
-    task_ids = fields.Char()
+    start_date = fields.Date()
+    due_date = fields.Date()
+    task_ids = fields.One2many(
+        string=u'Danh sach cac Task',
+        comodel_name='exam.task',
+    )
