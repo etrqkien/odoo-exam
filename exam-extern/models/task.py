8fc1c4a26cfc972944086d1c14484baa21096868
ApolloOne <trang78952@gmail.com>
2019-01-05 08:54:05 +0700

    exam-extern

--- exam-extern/models/task.py
+++ exam-extern/models/task.py
@@ -1,16 +1,27 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api
-import datetime
+
 
 class Task(models.Model):
     _inherit = 'exam.task'
-    status = fields.Selection([('init', 'khoi tao'), ('inprogess', 'dang lam viec'), ('finish', 'kethuc')])
+    status = fields.Selection(selection=[('init', 'khoi tao'), ('inprogess', 'dang lam viec'), ('finish', 'kethuc')])
     working_status = fields.Selection(
-        [('bd', 'not start'), ('dang', 'in working time'), ('kt', 'finish'), ('qua han', 'over deadline')],
-        compute='caculate_date')
+        selection=[('bd', 'not start'), ('dang', 'in working time'), ('kt', 'finish'), ('quahan', 'over deadline')],
+        compute='working_status_compute'
+    )
+    @api.onchange('status','working_status')
+    def working_status_compute(self):
+        if self.start_date and self.due_date:
 
-    @api.onchange('status', 'working_status')
-    def caculate_date(self):
+            sdate = fields.Date.from_string(self.start_date)
+            ddate = fields.Date.from_string(self.due_date)
+            today = fields.Date.from_string(fields.Date.today())
 
-        if (self.status == 'init'):
-            self.working_status = 'bd'
+            if self.status == 'finish':
+                self.working_status = 'kt'
+            elif today < sdate:
+                self.working_status = 'bd'
+            elif sdate <= today <= ddate:
+                self.working_status = 'dang'
+            else:
+                self.working_status = 'quahan'
